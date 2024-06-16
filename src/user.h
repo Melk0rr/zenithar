72e37ccd8d4e04277354e2b63e64a28606060498
Me1k0r <runner-me1k0r@proton.me>
2024-06-16 17:57:27 +0200

    Add: function header for user:addExpense

--- src/user.h
+++ src/user.h
@@ -8,4 +8,13 @@ typedef struct user
 {
   signed char userName[256];
   expense *expenseList;
+  int expenseCount = 0;
 } user;
+
+/**
+ * @brief Adds a new expense for the given user
+ * 
+ * @param usr {user} : user to whom add the new expense
+ * @param exp {expense} : new expense to add
+ */
+void addExpense(user *usr, expense exp);
