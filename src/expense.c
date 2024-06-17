03198f1f5c89a1b028e98c6655cf08402b6dd60f
Me1k0r <runner-me1k0r@proton.me>
2024-06-17 19:11:46 +0200

    update: create expense function

--- src/expense.c
+++ src/expense.c
@@ -1,1 +1,24 @@
 #include "expense.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+expense *createExpense(const signed char *expName, int cost)
+{
+  // Allocate memory
+  expense *newExpense = (expense *)malloc(sizeof(expense));
+
+  if (newExpense == NULL)
+  {
+    fprintf(stderr, "createExpense::Memory allocation failed !");
+    exit(1);
+  }
+
+  // Change expense name and ensure null-termination
+  strncpy(newExpense->expenseName, expName, sizeof(newExpense->expenseName) - 1);
+  newExpense->expenseName[sizeof(newExpense->expenseName) - 1] = '\0';
+
+  newExpense->expenseCost = cost;
+
+  return newExpense;
+}
--- src/expense.h
+++ src/expense.h
@@ -8,3 +8,12 @@ typedef struct expense
   signed char expenseName[256];
   int expenseCost;
 } expense;
+
+/**
+ * @brief Create a Expense struct
+ * 
+ * @param expName {signed char} name of the new expense
+ * @param cost {int} cost of the expense
+ * @return struct expense* 
+ */
+expense *createExpense(const signed char *expName, int cost);
