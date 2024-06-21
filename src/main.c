be3cce5256d1851ba98398d9009850c6306f40cf
Me1k0r <runner-me1k0r@proton.me>
2024-06-21 18:54:19 +0200

    update: tweaked test in main

--- src/main.c
+++ src/main.c
@@ -1,8 +1,30 @@
+#include "group.h"
+#include "zenithar.h"
+
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char const *argv[])
 {
-    printf("Hello World!\n");
+	signed char *groupName = getSecureInputSignedChar("Group name: ", 256);
+	group *testGroup = createGroup(groupName);
 
-    return 0;
+	int numberOfMembers = getSecureInputInt("How many users are in this group ? ", 4);
+
+	for (int i = 0; i < numberOfMembers; i++)
+	{
+		char buffer[50];
+		sprintf(buffer, "What is the name of user %d ? ", i + 1);
+
+		signed char *currentUsrName = getSecureInputSignedChar(buffer, 256);
+		user *currentUsr = createUser(currentUsrName);
+		
+		addGroupMember(testGroup, currentUsr);
+	}
+	
+	printf("%s : %d\n", testGroup->groupName, testGroup->memberCount);
+
+	free(testGroup->members);
+	free(testGroup);
+	return 0;
 }
