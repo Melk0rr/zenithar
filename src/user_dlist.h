b4f142198a0bb4081e8aaedc91663b10483aff52
Me1k0r <runner-me1k0r@proton.me>
2024-07-21 23:01:49 +0200

    edit: user dlist structures

--- src/user_dlist.h
+++ src/user_dlist.h
@@ -1,4 +1,35 @@
 #ifndef __USER_DLIST__H__
 #define __USER_DLIST__H__
 
+#import "user.h"
+#import "utils.h"
+
+// #########################################################
+// User DList and node struct definition
+
+/**
+ * @brief Users doubled chained list node
+ * 
+ */
+typedef struct UserDListNode
+{
+  user nodeUser;
+  struct UserDListNode *prev;
+  struct UserDListNode *next;
+} UserDListNode;
+
+/**
+ * @brief User doubled chained list
+ * 
+ */
+typedef struct UserDList
+{
+  int length;
+  struct UserDListNode *begin;
+  struct UserDListNode *end;
+} *UserDList;
+
+// #########################################################
+// Prototypes
+
 #endif
