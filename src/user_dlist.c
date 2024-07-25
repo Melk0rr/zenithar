fc7200a2daf10892d548b58fa2480e9ab6e7ce5d
Me1k0r <runner-me1k0r@proton.me>
2024-07-25 16:14:27 +0200

    edit: user dlist - implementation of isUserDListEmpty

--- src/user_dlist.c
+++ src/user_dlist.c
@@ -1,9 +1,22 @@
-#include "user_dlist.h" 
+#include "user_dlist.h"
 
+#include <stdio.h>
+#include <stdlib.h>
 
 // Function to create an empty user dlist : see user_dlist.h
 UserDList newUserDList()
 {
   UserDList uli;
   return uli;
+}
+
+// Function check if a User dlist is empty : see user_dlist.h
+bool isUserDListEmpty(UserDList uli)
+{
+  if (uli == NULL)
+  {
+    return true;
+  }
+  
+  return false;
 }
