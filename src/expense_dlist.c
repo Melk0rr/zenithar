7d6aa2fff0b34e42602e5887a68ccde1d389428b
Me1k0r <runner-me1k0r@proton.me>
2025-08-02 21:36:49 +0200

    edit: expense_dlist - includes

--- src/expense_dlist.c
+++ src/expense_dlist.c
@@ -3,8 +3,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "expense_dlist.h"
 #include "expense.h"
+#include "expense_dlist.h"
 
 // Function to create an empty expense dlist : see expense_dlist.h
 ExpenseDList * newExpenseDList()
