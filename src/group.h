e08def4c0c3b9a4a79e387d3320bb719770556e4
Me1k0r <runner-me1k0r@proton.me>
2025-08-03 21:50:57 +0200

    edit: group - format 1

--- src/group.h
+++ src/group.h
@@ -9,7 +9,7 @@
 
 /**
  * @brief Group of users to share expenses
- * 
+ *
  */
 typedef struct group
 {
@@ -17,7 +17,7 @@ typedef struct group
   char groupName[25];
 
   // Member list which contains users
-  UserDList *members;
+  UserDList * members;
 } group;
 
 // #########################################################
@@ -25,7 +25,7 @@ typedef struct group
 
 /**
  * @brief Create a new Group
- * 
+ *
  * @param grpName {const signed char} : group name
  * @param members {user} : members of the group
  * @param numberOfMembers {int} : number of members
@@ -35,7 +35,7 @@ group * createGroup(char const * const grpName);
 
 /**
  * @brief Initialize a group's member list
- * 
+ *
  * @param grp {group} : targeted group
  * @param members {user} : member list
  * @param numberOfMembers {int} : number of members for memory allocation
@@ -44,15 +44,16 @@ void initGroupMembers(group * const grp, user * const members, size_t const numb
 
 /**
  * @brief Adds a user into the group's member list
- * 
+ *
  * @param grp {group} : targeted group
  * @param newMember {user} : user to add
  */
 void addGroupMember(group * const grp, user * const newMember);
 
 /**
- * @brief Creates a new user with given username and adds it into the group's member list
- * 
+ * @brief Creates a new user with given username and adds it into the group's
+ * member list
+ *
  * @param grp {group} : group in which the new user will be added
  * @param userName {const signed char *} : username for the new user
  */
@@ -60,7 +61,7 @@ void addNewGroupMember(group * const grp, char const * userName);
 
 /**
  * @brief Removes the given user from members of the provided group
- * 
+ *
  * @param grp {group} : group from which the member will be removed
  * @param member {usr} : member to remove
  */
@@ -68,28 +69,28 @@ void removeGroupMember(group * const grp, user * const member);
 
 /**
  * @brief Clears group members
- * 
+ *
  * @param grp {group} : the group the members will be cleared
  */
 void clearGroupMembers(group * const grp);
 
 /**
  * @brief Prints group member name
- * 
+ *
  * @param grp {group} : group to print member of
  */
 void printMemberNames(group * const grp);
 
 /**
  * @brief Prints the balance for each group member
- * 
+ *
  * @param grp {group} : target group
  */
 void getGroupBalance(group * const grp);
 
 /**
  * @brief Resets expenses of group members
- * 
+ *
  * @param grp {group} : target group
  */
 void resetGroupExpenses(group * const grp);
